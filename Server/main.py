eb3517e50cf6883e336390bc279423ca29c53f7e
congaductq <ducdvse03458@fpt.edu.vn>
2016-11-01 11:39:38 +0800

    Server update

--- Server/main.py
+++ Server/main.py
@@ -7,25 +7,7 @@ if __name__ == "__main__":
     # local = raw_input("Enter your localhost:")
     local = 'localhost'
     ip = 'http://' + local + '/patient-tracking-web/api/web/user/alert'
-    print 'Started'
+    print('Started')
     def batch():
-        cnx = mysql.connector.connect(user='sa', password='abcd1234',
-                                      host='localhost:3',
-                                      database='patient_tracking')
-        cursor = cnx.cursor()
-        cursor.execute("""
-                select resident_id, floor_id
-                from location as rl
-                where created_at < DATE_SUB(NOW(), INTERVAL 6 second)
-                """)
-        results = cursor.fetchall()
-        for row in results:
-            resident_id = row[0]
-            last_position = row[1]
-            ipa = ip + '?resident_id=' + str(row[0]) + '&last_position=' + str(row[1]) + '&type=3'
-            print 'Alert resident id: ' + str(resident_id) + ' at position ' + str(last_position)
-            r = requests.post(ipa)
-        cursor.close()
-        cnx.close()
         threading.Timer(SEC, batch).start()
     batch()
